e1c21e8f3e16d5fae2ef13d5ee0e9a148694d4f4
Robert Jarrett <howtotech77@gmail.com>
2024-10-18 07:20:53 -0500

    Linkvertise Ad Banner
    Added Linkvertise Ad Banner to Code

--- src/app/page.js
+++ src/app/page.js
@@ -70,7 +70,7 @@ async function Home() {
         <div
         >
 /*Linkvertise Ad Banner*/
-        <div style="position: relative;"><iframe src="https://publisher.linkvertise.com/cdn/ads/LV-468x60/index.html" frameborder="0" height="60" width="468"></iframe><a href="https://publisher.linkvertise.com/ac/101337" target="_blank" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;"></a></div>
+        <iframe src="https://publisher.linkvertise.com/cdn/ads/LV-468x60/index.html" frameborder="0" height="60" width="468"></iframe><a href="https://publisher.linkvertise.com/ac/101337" target="_blank" style="position: absolute; top: 0; bottom: 0; left: 0; right: 0;"></a>
     /*Linkvertise Ad Banner End*/
     
           <Animecard data={herodata} cardid="Trending Now" />
