38c4952f161b0a7f5ca142f8e273c9ba34e7b5f1
Robert Jarrett <howtotech77@gmail.com>
2024-12-18 16:34:54 -0500

    Update source.js

--- src/actions/source.js
+++ src/actions/source.js
@@ -38,11 +38,11 @@ export async function getZoroSources(id, provider, episodeid, epnum, subtype) {
 }
 export async function getAnimeSources(id, provider, epid, epnum, subtype) {
     try {
-        if (provider === "kuro") {
+        if (provider === "gogoanime") {
             const data = await getGogoSources(epid);
             return data;
         }
-        if (provider === "roro") {
+        if (provider === "zoro") {
             const data = await getZoroSources(id, provider, epid, epnum, subtype)
             return data;
         }
