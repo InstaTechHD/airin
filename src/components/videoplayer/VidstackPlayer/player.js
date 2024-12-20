05c8ee0da48edc20d1e46fab827e87033658012f
Robert Jarrett <howtotech77@gmail.com>
2024-12-20 13:56:53 -0500

    Update player.js

--- src/components/videoplayer/VidstackPlayer/player.js
+++ src/components/videoplayer/VidstackPlayer/player.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useRef, useState } from "react";
 import "@vidstack/react/player/styles/base.css";
 import styles from "./player.module.css";
@@ -11,16 +11,16 @@ import {
   TextTrack,
 } from "@vidstack/react";
 import { useRouter } from "next/navigation";
-import VideoProgressSave from '../../../utils/VideoProgressSave';
+import VideoProgressSave from "../../../utils/VideoProgressSave";
 import { VideoLayout } from "./components/layouts/video-layout";
-import { DefaultKeyboardDisplay } from '@vidstack/react/player/layouts/default';
-import '@vidstack/react/player/styles/default/keyboard.css';
+import { DefaultKeyboardDisplay } from "@vidstack/react/player/layouts/default";
+import "@vidstack/react/player/styles/default/keyboard.css";
 import { updateEp } from "@/lib/EpHistoryfunctions";
 import { saveProgress } from "@/lib/AnilistUser";
-import { FastForwardIcon, FastBackwardIcon } from '@vidstack/react/icons';
-import { useSettings, useTitle, useNowPlaying } from '@/lib/store';
+import { FastForwardIcon, FastBackwardIcon } from "@vidstack/react/icons";
+import { useSettings, useTitle, useNowPlaying } from "@/lib/store";
 import { useStore } from "zustand";
-import { toast } from 'sonner';
+import { toast } from "sonner";
 
 function Player({ dataInfo, id, groupedEp, src, session, savedep, subtitles, thumbnails, skiptimes }) {
   const settings = useStore(useSettings, (state) => state.settings);
@@ -43,7 +43,6 @@ function Player({ dataInfo, id, groupedEp, src, session, savedep, subtitles, thu
 
   useEffect(() => {
     playerRef.current?.subscribe(({ currentTime, duration }) => {
-
       if (skiptimes && skiptimes.length > 0) {
         const opStart = skiptimes[0]?.startTime ?? 0;
         const opEnd = skiptimes[0]?.endTime ?? 0;
@@ -68,21 +67,20 @@ function Player({ dataInfo, id, groupedEp, src, session, savedep, subtitles, thu
           }
         }
       }
-    })
-
+    });
   }, [settings]);
 
   function onCanPlay() {
     if (skiptimes && skiptimes.length > 0) {
       const track = new TextTrack({
-        kind: 'chapters',
+        kind: "chapters",
         default: true,
-        label: 'English',
-        language: 'en-US',
-        type: 'json'
+        label: "English",
+        language: "en-US",
+        type: "json",
       });
       for (const cue of skiptimes) {
-        track.addCue(new window.VTTCue(Number(cue.startTime), Number(cue.endTime), cue.text))
+        track.addCue(new window.VTTCue(Number(cue.startTime), Number(cue.endTime), cue.text));
       }
       playerRef.current.textTracks.add(track);
     }
@@ -121,8 +119,12 @@ function Player({ dataInfo, id, groupedEp, src, session, savedep, subtitles, thu
           aniId: String(dataInfo?.id) || String(id),
           aniTitle: dataInfo?.title?.[animetitle] || dataInfo?.title?.romaji,
           epTitle: currentep?.title || `EP ${epNum}`,
-          image: currentep?.img || currentep?.image ||
-            dataInfo?.bannerImage || dataInfo?.coverImage?.extraLarge || '',
+          image:
+            currentep?.img ||
+            currentep?.image ||
+            dataInfo?.bannerImage ||
+            dataInfo?.coverImage?.extraLarge ||
+            "",
           epId: epId,
           epNum: Number(epNum) || Number(currentep?.number),
           timeWatched: currentTime,
@@ -130,15 +132,19 @@ function Player({ dataInfo, id, groupedEp, src, session, savedep, subtitles, thu
           provider: provider,
           nextepId: nextep?.id || null,
           nextepNum: nextep?.number || null,
-          subtype: subtype
-        })
+          subtype: subtype,
+        });
 
         UpdateVideoProgress(dataInfo?.id || id, {
           aniId: String(dataInfo?.id) || String(id),
           aniTitle: dataInfo?.title?.[animetitle] || dataInfo?.title?.romaji,
           epTitle: currentep?.title || `EP ${epNum}`,
-          image: currentep?.img || currentep?.image ||
-            dataInfo?.bannerImage || dataInfo?.coverImage?.extraLarge || '',
+          image:
+            currentep?.img ||
+            currentep?.image ||
+            dataInfo?.bannerImage ||
+            dataInfo?.coverImage?.extraLarge ||
+            "",
           epId: epId,
           epNum: Number(epNum) || Number(currentep?.number),
           timeWatched: currentTime,
@@ -159,77 +165,17 @@ function Player({ dataInfo, id, groupedEp, src, session, savedep, subtitles, thu
     };
   }, [isPlaying, duration]);
 
-  function onLoadedMetadata() {
-    if (savedep && savedep[0]) {
-      const seekTime = savedep[0]?.timeWatched;
-      if (seekTime) {
-        remote.seek(seekTime - 3);
-      }
-    }
-    else {
-      const seek = getVideoProgress(dataInfo?.id);
-      if (seek?.epNum === Number(epNum)) {
-        const seekTime = seek?.timeWatched;
-        const percentage = duration !== 0 ? seekTime / Math.round(duration) : 0;
-
-        if (percentage >= 0.95) {
-          remote.seek(0);
-        } else {
-          remote.seek(seekTime - 3);
-        }
-      }
-    }
-  }
-
-  function onTimeUpdate() {
-    const currentTime = playerRef.current?.currentTime;
-    const timeToShowButton = duration - 8;
-    const percentage = currentTime / duration;
-
-    if (session && !progressSaved && percentage >= 0.9) {
-      try {
-        setprogressSaved(true); // Mark progress as saved
-        saveProgress(session.user.token, dataInfo?.id || id, Number(epNum) || Number(currentep?.number));
-      } catch (error) {
-        console.error("Error saving progress:", error);
-        toast.error("Error saving progress due to high traffic.");
-      }
-    }
-
-    const nextButton = document.querySelector(".nextbtn");
-
-    if (nextButton) {
-      if (duration !== 0 && (currentTime > timeToShowButton && nextep?.id)) {
-        nextButton.classList.remove("hidden");
-      } else {
-        nextButton.classList.add("hidden");
-      }
-    }
-  }
-
-  function onSourceChange() {
-    if(fullscreen){
-      console.log("true")
-    }else{
-      console.log("false")
-    }
-  }
-
-  function handleop() {
-    console.log("Skipping Intro");
-    Object.assign(playerRef.current ?? {}, { currentTime: skiptimes[0]?.endTime ?? 0 });
-  }
-
-  function handleed() {
-    console.log("Skipping Outro");
-    Object.assign(playerRef.current ?? {}, { currentTime: skiptimes[1]?.endTime ?? 0 });
-  }
-
   return (
     <div>
-      <MediaPlayer key={src} ref={playerRef} playsInline aspectRatio={16 / 9} load={settings?.load || 'idle'} muted={settings?.audio || false}
+      <MediaPlayer
+        key={src}
+        ref={playerRef}
+        playsInline
+        aspectRatio={16 / 9}
+        load={settings?.load || "idle"}
+        muted={settings?.audio || false}
         autoPlay={settings?.autoplay || false}
-        title={currentep?.title || `EP ${epNum}` || 'Loading...'}
+        title={currentep?.title || `EP ${epNum}` || "Loading..."}
         className={`${styles.player} player relative`}
         crossOrigin={"anonymous"}
         streamType="on-demand"
@@ -237,59 +183,23 @@ function Player({ dataInfo, id, groupedEp, src, session, savedep, subtitles, thu
         onEnd={onEnd}
         onEnded={onEnded}
         onCanPlay={onCanPlay}
-        src={{
-          src: `${process.env.NEXT_PUBLIC_PROXY_URI}/fetch?url=${encodeURIComponent(src)}&ref=${encodeURIComponent("https://s3embtaku.pro")}`,
-          type: "application/x-mpegurl",
-        }}
-        onPlay={onPlay}
-        onPause={onPause}
-        onLoadedMetadata={onLoadedMetadata}
-        onTimeUpdate={onTimeUpdate}
-        onSourceChange={onSourceChange}
       >
         <MediaProvider>
-          {subtitles && subtitles?.map((track) => (
-            <Track {...track} key={track.src} />
-          ))}
+          {subtitles &&
+            subtitles?.map((track) => <Track {...track} key={track.src} />)}
         </MediaProvider>
-        {opbutton && <button onClick={handleop} className='absolute bottom-[70px] sm:bottom-[83px] right-4 z-[40] bg-white text-black py-2 px-3 rounded-[6px] font-medium text-[15px]'>Skip Opening</button>}
-        {edbutton && <button onClick={handleed} className='absolute bottom-[70px] sm:bottom-[83px] right-4 z-[40] bg-white text-black py-2 px-3 rounded-[6px] font-medium text-[15px]'>Skip Ending</button>}
-        <VideoLayout
-          subtitles={subtitles}
-          thumbnails={thumbnails ? process.env.NEXT_PUBLIC_PROXY_URI + '/' + thumbnails[0]?.src : ""}
-          groupedEp={groupedEp}
-        />
-        <DefaultKeyboardDisplay
-          icons={{
-            Play: null,
-            Pause: null,
-            Mute: null,
-            VolumeUp: null,
-            VolumeDown: null,
-            EnterFullscreen: null,
-            ExitFullscreen: null,
-            EnterPiP: null,
-            ExitPiP: null,
-            CaptionsOn: null,
-            CaptionsOff: null,
-            SeekForward: FastForwardIcon,
-            SeekBackward: FastBackwardIcon,
-          }}
-        />
+        <VideoLayout groupedEp={groupedEp} />
+        <DefaultKeyboardDisplay />
       </MediaPlayer>
       <div className={styles.iconContainer}>
-        <a href={`https://whouphesaussums.net/?url=${encodeURIComponent(src)}`} download className={styles.icon}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Download_icon.svg" alt="Download" style={{ width: "172px", height: "172px" }} />
+        <a
+          href={`https://whouphesaussums.net/?url=${encodeURIComponent(src)}`}
+          download
+        >
+          Download
         </a>
-        <a href={`https://anilist.co/anime/${id}`} className={styles.icon}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/Anilist_Logo.svg" alt="AniList" style={{ width: "172px", height: "172px" }} />
-        </a>
-        <a href={`https://myanimelist.net/anime/${id}`} className={styles.icon}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/MyAnimeList_Logo_Small.png" alt="MyAnimeList" style={{ width: "172px", height: "172px" }} />
-        </a>
+        <a href={"anilist URL from"}>Click btnmal lst</a>
       </div>
     </div>
-  )
+  );
 }
-
-export default Player
