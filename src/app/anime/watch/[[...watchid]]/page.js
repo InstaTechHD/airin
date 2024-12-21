0c9e64b1c2d24c8ef68415072c1a3fcfed793a2f
Robert Jarrett <howtotech77@gmail.com>
2024-12-20 21:38:16 -0500

    Update page.js

--- src/app/anime/watch/[[...watchid]]/page.js
+++ src/app/anime/watch/[[...watchid]]/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { AnimeInfoAnilist } from '@/lib/Anilistfunctions';
+import { generateMetadata } from './metadata'; // Import generateMetadata
 import NextAiringDate from "@/components/videoplayer/NextAiringDate";
 import PlayerAnimeCard from "@/components/videoplayer/PlayerAnimeCard";
 import Navbarcomponent from "@/components/navbar/Navbar";
@@ -10,54 +10,7 @@ import Animecards from "@/components/CardComponent/Animecards";
 import RandomTextComponent from "@/components/RandomTextComponent";
 import { createWatchEp, getEpisode } from "@/lib/EpHistoryfunctions";
 import { getAuthSession } from "../../../api/auth/[...nextauth]/route";
-import { redis } from '@/lib/rediscache';
 import CommentSection from './CommentSection'; // Import CommentSection
-
-async function getInfo(id) {
-  try {
-    let cachedData;
-    if (redis) {
-      cachedData = await redis.get(`info:${id}`);
-      if (!JSON.parse(cachedData)) {
-        await redis.del(`info:${id}`);
-        cachedData = null;
-      }
-    }
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    } else {
-      const data = await AnimeInfoAnilist(id);
-      const cacheTime = data?.nextAiringEpisode?.episode ? 60 * 60 * 2 : 60 * 60 * 24 * 45;
-      if (redis && data !== null && data) {
-        await redis.set(`info:${id}`, JSON.stringify(data), "EX", cacheTime);
-      }
-      return data;
-    }
-  } catch (error) {
-    console.error("Error fetching info: ", error);
-  }
-}
-
-export async function generateMetadata({ params, searchParams }) {
-  const id = searchParams?.id;
-  const data = await getInfo(id);
-  const epnum = searchParams?.ep;
-
-  return {
-    title: "Episode " + epnum + ' - ' + (data?.title?.english || data?.title?.romaji || 'Loading...'),
-    description: data?.description?.slice(0, 180),
-    openGraph: {
-      title: "Episode " + epnum + ' - ' + (data?.title?.english || data?.title?.romaji),
-      images: [data?.coverImage?.extraLarge],
-      description: data?.description,
-    },
-    twitter: {
-      card: "summary",
-      title: "Episode " + epnum + ' - ' + (data?.title?.english || data?.title?.romaji),
-      description: data?.description?.slice(0, 180),
-    },
-  }
-}
 
 export async function Ephistory(session, aniId, epNum) {
   try {
