4d7813694df77e2ed2b0881977375981ea3229b9
Robert Jarrett <howtotech77@gmail.com>
2024-12-20 19:25:11 -0500

    Update mappings.js

--- src/actions/mappings.js
+++ src/actions/mappings.js
@@ -1,4 +1,131 @@
+"use server"
+import { redis } from '@/lib/rediscache';
+import { ANIME } from "@consumet/extensions";
+import { AnimeInfoAnilist } from '@/lib/Anilistfunctions';
+import { findSimilarTitles } from '@/lib/stringSimilarity';
+
+const gogo = new ANIME.Gogoanime();
+const hianime = new ANIME.Zoro();
 const anipahe = new ANIME.Anipahe();
+
+export async function getMappings(anilistId) {
+    const data = await getInfo(anilistId);
+    let gogores, zorores, anipaheRes;
+
+    if (!data) {
+        return null;
+    }
+
+    gogores = await mapGogo(data?.title);
+    zorores = await mapZoro(data?.title);
+    anipaheRes = await mapAnipahe(data?.title);
+
+    return { 
+        gogoanime: gogores, 
+        zoro: zorores, 
+        anipahe: anipaheRes, 
+        id: data?.id, 
+        malId: data?.idMal, 
+        title: data?.title.romaji 
+    };
+}
+
+async function getInfo(id) {
+    try {
+        let cachedData;
+        if (redis) {
+            cachedData = await redis.get(`info:${id}`);
+            if (!JSON.parse(cachedData) || JSON.parse(cachedData)?.length === 0) {
+                await redis.del(`info:${id}`);
+                cachedData = null;
+            }
+        }
+        if (cachedData) {
+            return JSON.parse(cachedData);
+        } else {
+            const data = await AnimeInfoAnilist(id);
+            const cacheTime = data?.status === 'FINISHED' ? 60 * 60 * 24 * 45 : 60 * 60 * 3;
+            if (redis && data !== null && data) {
+                await redis.set(`info:${id}`, JSON.stringify(data), "EX", cacheTime);
+            }
+            return data;
+        }
+    } catch (error) {
+        console.error("Error fetching info: ", error);
+    }
+}
+
+async function mapGogo(title) {
+    let eng = await gogo.search(title?.english || title?.userPreferred);
+    let rom = await gogo.search(title?.romaji);
+    let english_search = eng?.results || [];
+    let romaji_search = rom?.results || [];
+
+    const combined = [...english_search, ...romaji_search];
+
+    const uniqueResults = Array.from(new Set(combined.map(item => JSON.stringify(item))))
+        .map(item => JSON.parse(item));
+
+    const gogomap = findSimilarTitles(title?.romaji || title?.english || title?.userPreferred, uniqueResults);
+    const gogoanime = {};
+
+    gogomap?.forEach((obj) => {
+        const title = obj.title;
+        const id = obj.id;
+
+        const match = title.replace(/TV/g, "").match(/([^)0-9]+)/);
+
+        if (match && (match[1].toLowerCase() === 'uncensored' || match[1].toLowerCase() === 'dub')) {
+            const key = match[1].replace(/\s+/g, '-').toLowerCase();
+            if (!gogoanime[key]) {
+                gogoanime[key] = id;
+            }
+        } else {
+            if (!gogoanime['sub']) {
+                gogoanime['sub'] = id;
+            }
+        }
+    });
+    return gogoanime;
+}
+
+async function mapZoro(title) {
+    let eng = await hianime.search(title?.english || title?.romaji || title?.userPreferred);
+    const zoromap = findSimilarTitles(title?.english, eng?.results);
+    const zoromaprom = findSimilarTitles(title?.romaji, eng?.results);
+    const combined = [...zoromap, ...zoromaprom];
+
+    const uniqueCombined = combined.reduce((acc, current) => {
+        const x = acc.find(item => item.id === current.id);
+        if (!x) {
+            return acc.concat([current]);
+        } else {
+            return acc;
+        }
+    }, []);
+
+    uniqueCombined.sort((a, b) => b.similarity - a.similarity);
+
+    const zoro = {};
+
+    uniqueCombined.forEach((obj) => {
+        const title = obj.title;
+        const id = obj.id;
+
+        const match = title.replace(/TV/g, "").match(/([^)0-9]+)/);
+        if (match && (match[1].toLowerCase() === 'uncensored' || match[1].toLowerCase() === 'dub')) {
+            const key = match[1].replace(/\s+/g, '-').toLowerCase();
+            if (!zoro[key]) {
+                zoro[key] = id;
+            }
+        } else {
+            if (!zoro['sub']) {
+                zoro['sub'] = id;
+            }
+        }
+    });
+    return zoro;
+}
 
 async function mapAnipahe(title) {
     let eng = await anipahe.search(title?.english || title?.romaji || title?.userPreferred);
@@ -22,7 +149,7 @@ async function mapAnipahe(title) {
         const title = obj.title;
         const id = obj.id;
 
-        const match = title.replace(/\(TV\)/g, "").match(/\(([^)0-9]+)\)/);
+        const match = title.replace(/TV/g, "").match(/([^)0-9]+)/);
         if (match && (match[1].toLowerCase() === 'uncensored' || match[1].toLowerCase() === 'dub')) {
             const key = match[1].replace(/\s+/g, '-').toLowerCase();
             if (!anipahe[key]) {
@@ -35,16 +162,4 @@ async function mapAnipahe(title) {
         }
     });
     return anipahe;
-}
-
-export async function getMappings(anilistId) {
-    const data = await getInfo(anilistId);
-    let gogores, zorores, anipaheRes;
-    if (!data) {
-        return null;
-    }
-    gogores = await mapGogo(data?.title);
-    zorores = await mapZoro(data?.title);
-    anipaheRes = await mapAnipahe(data?.title);
-    return { gogoanime: gogores, zoro: zorores, anipahe: anipaheRes, id: data?.id, malId: data?.idMal, title: data?.title.romaji };
 }
