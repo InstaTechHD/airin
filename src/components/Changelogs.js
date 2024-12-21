f82882499dedb0e23999fcc257a0aa19a7f23c18
Robert Jarrett <howtotech77@gmail.com>
2024-12-20 19:48:24 -0500

    Update Changelogs.js

--- src/components/Changelogs.js
+++ src/components/Changelogs.js
@@ -100,7 +100,7 @@ export default function Changelogs() {
                                         <div className="flex gap-3 items-center">
                                             {/* Github Icon */}
                                             <Link
-                                                href="https://github.com/Noname968/airin"
+                                                href="https://github.com/InstaTechHD/airin"
                                                 target="_blank"
                                                 className="w-5 h-5 hover:opacity-75"
                                             >
@@ -130,7 +130,7 @@ export default function Changelogs() {
                                             </Link>
                                             {/* Discord Icon */}
                                             <Link
-                                                href="https://discord.gg/QnbFaudJNf"
+                                                href="https://discord.gg/zvD8xDhqzu"
                                                 target="_blank"
                                                 className="w-6 h-6 hover:opacity-75"
                                             >
