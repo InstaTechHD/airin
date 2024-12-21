ff1f6c4816b3807b613449b719605ce46a81ec99
Robert Jarrett <howtotech77@gmail.com>
2024-12-20 19:38:14 -0500

    Update episode.js

--- src/actions/episode.js
+++ src/actions/episode.js
@@ -1,5 +1,4 @@
 "use server";
-import axios from 'axios';
 import { ANIME } from "@consumet/extensions";
 import { CombineEpisodeMeta } from "@/utils/EpisodeFunctions";
 import { redis } from "@/lib/rediscache";
@@ -7,11 +6,11 @@ import { getMappings } from "./mappings";
 
 const gogo = new ANIME.Gogoanime();
 const zoro = new ANIME.Zoro();
-const weebApiBaseUrl = 'https://weebapi.onrender.com';
 
 export async function fetchGogoEpisodes(id) {
   try {
     const data = await gogo.fetchAnimeInfo(id);
+
     return data?.episodes || [];
   } catch (error) {
     console.error("Error fetching gogoanime:", error.message);
@@ -22,6 +21,7 @@ export async function fetchGogoEpisodes(id) {
 export async function fetchZoroEpisodes(id) {
   try {
     const data = await zoro.fetchAnimeInfo(id);
+
     return data?.episodes || [];
   } catch (error) {
     console.error("Error fetching zoro:", error.message);
@@ -29,12 +29,23 @@ export async function fetchZoroEpisodes(id) {
   }
 }
 
-export async function fetchAnipaheEpisodes(id) {
+async function fetchEpisodeMeta(id, available = false) {
   try {
-    const response = await axios.get(`${weebApiBaseUrl}/get_full_data/${id}`);
-    return response.data.episodes || [];
+    if (available) {
+      return null;
+    }
+    const res = await fetch(
+      `https://api.ani.zip/mappings?anilist_id=${id}`
+    );
+const data = await res.json()
+    const episodesArray = Object.values(data?.episodes);
+
+    if (!episodesArray) {
+      return [];
+    }
+    return episodesArray;
   } catch (error) {
-    console.error("Error fetching anipahe episodes:", error.message);
+    console.error("Error fetching and processing meta:", error.message);
     return [];
   }
 }
@@ -51,11 +62,25 @@ const fetchAndCacheData = async (id, meta, redis, cacheTime, refresh) => {
 
   if (mappings) {
     if (mappings.gogoanime && Object.keys(mappings.gogoanime).length >= 1) {
-      subEpisodes = await fetchGogoEpisodes(
-        mappings.gogoanime.uncensored || mappings.gogoanime.sub || mappings.gogoanime.tv
-      );
-      dubEpisodes = await fetchGogoEpisodes(mappings.gogoanime.dub);
-      if (subEpisodes.length > 0 || dubEpisodes.length > 0) {
+      // Fetch sub episodes if available
+      if (
+        mappings?.gogoanime?.uncensored ||
+        mappings?.gogoanime?.sub ||
+        mappings?.gogoanime?.tv
+      ) {
+        subEpisodes = await fetchGogoEpisodes(
+          mappings?.gogoanime?.uncensored ||
+            mappings.gogoanime.sub ||
+            mappings?.gogoanime?.tv
+        );
+      }
+
+      // Fetch dub episodes if available
+      if (mappings?.gogoanime?.dub) {
+        dubEpisodes = await fetchGogoEpisodes(mappings?.gogoanime?.dub);
+      }
+
+      if (subEpisodes?.length > 0 || dubEpisodes?.length > 0) {
         allepisodes.push({
           episodes: { sub: subEpisodes, dub: dubEpisodes },
           providerId: "gogoanime",
@@ -63,55 +88,62 @@ const fetchAndCacheData = async (id, meta, redis, cacheTime, refresh) => {
         });
       }
     }
-    if (mappings.zoro && Object.keys(mappings.zoro).length >= 1) {
-      subEpisodes = await fetchZoroEpisodes(
-        mappings.zoro.uncensored || mappings.zoro.sub || mappings.zoro.tv
-      );
-      if (subEpisodes.length > 0) {
+    if (mappings?.zoro && Object.keys(mappings.zoro).length >= 1) {
+      let subEpisodes = [];
+
+      // Fetch sub episodes if available
+      if (
+        mappings?.zoro?.uncensored ||
+        mappings?.zoro?.sub ||
+        mappings?.zoro?.tv
+      ) {
+        subEpisodes = await fetchZoroEpisodes(
+          mappings?.zoro?.uncensored
+            ? mappings?.zoro?.uncensored
+            : mappings.zoro.sub
+        );
+      }
+      if (subEpisodes?.length > 0) {
         const transformedEpisodes = subEpisodes.map(episode => ({
           ...episode,
           id: transformEpisodeId(episode.id)
         }));
+      
         allepisodes.push({
           episodes: transformedEpisodes,
           providerId: "zoro",
         });
       }
     }
-    if (mappings.anipahe && Object.keys(mappings.anipahe).length >= 1) {
-      subEpisodes = await fetchAnipaheEpisodes(
-        mappings.anipahe.uncensored || mappings.anipahe.sub || mappings.anipahe.tv
-      );
-      if (subEpisodes.length > 0) {
-        const transformedEpisodes = subEpisodes.map(episode => ({
-          ...episode,
-          id: transformEpisodeId(episode.id)
-        }));
-        allepisodes.push({
-          episodes: transformedEpisodes,
-          providerId: "anipahe",
-        });
-      }
-    }
-  }
+  } 
+  const cover = await fetchEpisodeMeta(id, !refresh)
 
-  const cover = await fetchEpisodeMeta(id, !refresh);
-
+  // Check if redis is available
   if (redis) {
-    if (allepisodes.length > 0) {
-      await redis.setex(`episode:${id}`, cacheTime, JSON.stringify(allepisodes));
+    if (allepisodes) {
+      await redis.setex(
+        `episode:${id}`,
+        cacheTime,
+        JSON.stringify(allepisodes)
+      );
     }
+
     let data = allepisodes;
     if (refresh) {
-      if (cover.length > 0) {
-        await redis.setex(`meta:${id}`, cacheTime, JSON.stringify(cover));
-        data = await CombineEpisodeMeta(allepisodes, cover);
+      if (cover && cover?.length > 0) {
+        try {
+          await redis.setex(`meta:${id}`, cacheTime, JSON.stringify(cover));
+          data = await CombineEpisodeMeta(allepisodes, cover);
+        } catch (error) {
+          console.error("Error serializing cover:", error.message);
+        }
       } else if (meta) {
         data = await CombineEpisodeMeta(allepisodes, JSON.parse(meta));
       }
     } else if (meta) {
       data = await CombineEpisodeMeta(allepisodes, JSON.parse(meta));
     }
+
     return data;
   } else {
     console.error("Redis URL not provided. Caching not possible.");
@@ -119,11 +151,82 @@ const fetchAndCacheData = async (id, meta, redis, cacheTime, refresh) => {
   }
 };
 
+export const getEpisodes = async (id, status, refresh = false) => {
+  let cacheTime = null;
+  if (status) {
+    cacheTime = 60 * 60 * 3;
+  } else {
+    cacheTime = 60 * 60 * 24 * 45;
+  }
+
+  let meta = null;
+  let cached;
+
+  if (redis) {
+    try {
+      // // Find keys matching the pattern "meta:*"
+      // const keys = await redis.keys("meta:*");
+
+      // // Delete keys matching the pattern "meta:*"
+      // if (keys.length > 0) {
+      //   await redis.del(keys);
+      //   console.log(`Deleted ${keys.length} keys matching the pattern "meta:*"`);
+      // }
+      meta = await redis.get(`meta:${id}`);
+      if (JSON.parse(meta)?.length === 0) {
+        await redis.del(`meta:${id}`);
+        console.log("deleted meta cache");
+        meta = null;
+      }
+      cached = await redis.get(`episode:${id}`);
+      if (JSON.parse(cached)?.length === 0) {
+        await redis.del(`episode:${id}`);
+        cached = null;
+      }
+      let data;
+      if (refresh) {
+        data = await fetchAndCacheData(id, meta, redis, cacheTime, refresh);
+      }
+      if (data?.length > 0) {
+        console.log("deleted cache");
+        return data;
+      }
+
+      console.log("using redis");
+    } catch (error) {
+      console.error("Error checking Redis cache:", error.message);
+    }
+  }
+
+  if (cached) {
+    try {
+      let cachedData = JSON.parse(cached);
+      if (meta) {
+        cachedData = await CombineEpisodeMeta(cachedData, JSON.parse(meta));
+      }
+      return cachedData;
+    } catch (error) {
+      console.error("Error parsing cached data:", error.message);
+    }
+  } else {
+    const fetchdata = await fetchAndCacheData(
+      id,
+      meta,
+      redis,
+      cacheTime,
+      !refresh
+    );
+    return fetchdata;
+  }
+};
+
+
 function transformEpisodeId(episodeId) {
   const regex = /^([^$]*)\$episode\$([^$]*)/;
   const match = episodeId.match(regex);
+
   if (match && match[1] && match[2]) {
-    return `${match[1]}?ep=${match[2]}`;
+    return `${match[1]}?ep=${match[2]}`; // Construct the desired output with the episode number
   }
-  return episodeId;
+  return episodeId; // Return original ID if no match is found
 }
