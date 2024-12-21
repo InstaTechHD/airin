a14c98d71aaf006ce2cd3bc44df7351971198466
Robert Jarrett <howtotech77@gmail.com>
2024-12-20 19:37:26 -0500

    Update mappings.js

--- src/actions/mappings.js
+++ src/actions/mappings.js
@@ -1,35 +1,22 @@
 "use server"
-import axios from 'axios';
 import { redis } from '@/lib/rediscache';
 import { ANIME } from "@consumet/extensions";
-import { AnimeInfoAnilist } from '@/lib/Anilistfunctions';
+import { AnimeInfoAnilist } from '@/lib/Anilistfunctions'
 import { findSimilarTitles } from '@/lib/stringSimilarity';
+
 
 const gogo = new ANIME.Gogoanime();
 const hianime = new ANIME.Zoro();
-const anipahe = new ANIME.Anipahe();
-const weebApiBaseUrl = 'https://weebapi.onrender.com';
 
 export async function getMappings(anilistId) {
     const data = await getInfo(anilistId);
-    let gogores, zorores, anipaheRes;
-
+    let gogores, zorores;
     if (!data) {
         return null;
     }
-
     gogores = await mapGogo(data?.title);
     zorores = await mapZoro(data?.title);
-    anipaheRes = await mapAnipahe(data?.title);
-
-    return { 
-        gogoanime: gogores, 
-        zoro: zorores, 
-        anipahe: anipaheRes, 
-        id: data?.id, 
-        malId: data?.idMal, 
-        title: data?.title.romaji 
-    };
+    return { gogoanime: gogores, zoro: zorores, id: data?.id, malId: data?.idMal, title: data?.title.romaji };
 }
 
 async function getInfo(id) {
@@ -43,6 +30,7 @@ async function getInfo(id) {
             }
         }
         if (cachedData) {
+            // console.log("using cached info")
             return JSON.parse(cachedData);
         } else {
             const data = await AnimeInfoAnilist(id);
@@ -62,13 +50,13 @@ async function mapGogo(title) {
     let rom = await gogo.search(title?.romaji);
     let english_search = eng?.results || [];
     let romaji_search = rom?.results || [];
-
+    // Combine both results and remove duplicates
     const combined = [...english_search, ...romaji_search];
 
     const uniqueResults = Array.from(new Set(combined.map(item => JSON.stringify(item))))
-        .map(item => JSON.parse(item));
+    .map(item => JSON.parse(item));
 
-    const gogomap = findSimilarTitles(title?.romaji || title?.english || title?.userPreferred, uniqueResults);
+    const gogomap = findSimilarTitles(title?.romaji || title?.english || title?.userPreferred, uniqueResults)
     const gogoanime = {};
 
     gogomap?.forEach((obj) => {
@@ -93,8 +81,8 @@ async function mapGogo(title) {
 
 async function mapZoro(title) {
     let eng = await hianime.search(title?.english || title?.romaji || title?.userPreferred);
-    const zoromap = findSimilarTitles(title?.english, eng?.results);
-    const zoromaprom = findSimilarTitles(title?.romaji, eng?.results);
+    const zoromap = findSimilarTitles(title?.english, eng?.results)
+    const zoromaprom = findSimilarTitles(title?.romaji, eng?.results)
     const combined = [...zoromap, ...zoromaprom];
 
     const uniqueCombined = combined.reduce((acc, current) => {
@@ -106,6 +94,7 @@ async function mapZoro(title) {
         }
     }, []);
 
+    // Sort based on similarity (assuming similarity is a property of the objects)
     uniqueCombined.sort((a, b) => b.similarity - a.similarity);
 
     const zoro = {};
@@ -127,41 +116,4 @@ async function mapZoro(title) {
         }
     });
     return zoro;
-}
-
-async function mapAnipahe(title) {
-    let eng = await axios.get(`${weebApiBaseUrl}/get_search_results/${encodeURIComponent(title?.english || title?.romaji || title?.userPreferred)}`);
-    const anipahemap = findSimilarTitles(title?.english, eng?.data.results);
-    const combined = [...anipahemap];
-
-    const uniqueCombined = combined.reduce((acc, current) => {
-        const x = acc.find(item => item.id === current.id);
-        if (!x) {
-            return acc.concat([current]);
-        } else {
-            return acc;
-        }
-    }, []);
-
-    uniqueCombined.sort((a, b) => b.similarity - a.similarity);
-
-    const anipahe = {};
-
-    anipahemap.forEach((obj) => {
-        const title = obj.title;
-        const id = obj.id;
-
-        const match = title.replace(/\(TV\)/g, "").match(/\(([^)0-9]+)\)/);
-        if (match && (match[1].toLowerCase() === 'uncensored' || match[1].toLowerCase() === 'dub')) {
-            const key = match[1].replace(/\s+/g, '-').toLowerCase();
-            if (!anipahe[key]) {
-                anipahe[key] = id;
-            }
-        } else {
-            if (!anipahe['sub']) {
-                anipahe['sub'] = id;
-            }
-        }
-    });
-    return anipahe;
 }
