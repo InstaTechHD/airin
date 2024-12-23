f293fda1a2535c90f074336a303c67714d9eb4c1
Robert Jarrett <howtotech77@gmail.com>
2024-12-23 14:22:00 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,42 +5,42 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   reloadOnOnline: true,
   swcMinify: true,
   disable: process.env.NODE_ENV === "development",
+  // disable:false,
   workboxOptions: {
     disableDevLogs: true,
   },
+  // ... other options you like
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "s4.anilist.co",
-      "artworks.thetvdb.com",
-      "media.kitsu.io",
-      "image.tmdb.org",
-    ],
-    unoptimized: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
-};
-
+    images: {
+        domains: ['s4.anilist.co','artworks.thetvdb.com','media.kitsu.io', 'image.tmdb.org'],
+        unoptimized: true
+      },
+    //   async headers() {
+    //     return [
+    //         {
+    //             source: "/api/:path*",
+    //             headers: [
+    //                 { key: "Access-Control-Allow-Credentials", value: "true" },
+    //                 { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+    //                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+    //                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    //             ]
+    //         }
+    //     ]
+    // },
+    typescript: {
+      // !! WARN !!
+      // Dangerously allow production builds to successfully complete even if
+      // your project has type errors.
+      // !! WARN !!
+      ignoreBuildErrors: true,
+    },
+  }
+   
 module.exports = withPWA(nextConfig);
-
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //   enabled: process.env.ANALYZE === 'true',
 // })
