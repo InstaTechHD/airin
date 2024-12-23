44c62e7cfd31bf7d32ab9dcb04fd8936ddcefde1
Robert Jarrett <howtotech77@gmail.com>
2024-12-23 15:43:35 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,29 +10,28 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   },
 });
 
-module.exports = function (webpackEnv) {
-  return {
-    ...withPWA({
-      images: {
-        domains: ['s4.anilist.co', 'artworks.thetvdb.com', 'media.kitsu.io', 'image.tmdb.org'],
-        unoptimized: true,
-      },
-      typescript: {
-        ignoreBuildErrors: true,
-      },
-    }),
-    webpack: (config, { isServer }) => {
-      if (!isServer) {
-        // Adding fallback for common Node.js modules
-        config.resolve.fallback = {
-          ...config.resolve.fallback,
-          fs: false,
-          child_process: false,
-          dns: false,
-          "fs/promises": false,
-        };
-      }
-      return config;
-    },
-  };
+module.exports = {
+  ...withPWA(),
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // For client-side only
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false, // Ensure that net module is false
+        tls: false, // Ensure that tls module is false
+        fs: false, // Ensure that fs module is false
+        "fs/promises": false, // Ensure that fs/promises module is false
+        child_process: false, // Ensure that child_process module is false
+        dns: false, // Ensure that dns module is false
+      };
+    }
+    return config;
+  },
+  images: {
+    domains: ['s4.anilist.co', 'artworks.thetvdb.com', 'media.kitsu.io', 'image.tmdb.org'],
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
