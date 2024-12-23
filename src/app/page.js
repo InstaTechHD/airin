cbda0b293fc083c67a0907f6917f1c431b1ad273
Robert Jarrett <howtotech77@gmail.com>
2024-12-23 16:12:26 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,48 +1,46 @@
-"use client"
+"use server"
 import Animecard from '@/components/CardComponent/Animecards'
 import Herosection from '@/components/home/Herosection'
 import Navbarcomponent from '@/components/navbar/Navbar'
-import { TrendingAnilist, PopularAnilist, Top100Anilist, SeasonalAnilist, CategoriesAnilist, HeroAnilist } from '@/lib/Anilistfunctions'
-import React, { useEffect, useState } from 'react'
+import { TrendingAnilist, PopularAnilist, Top100Anilist, SeasonalAnilist } from '@/lib/Anilistfunctions'
+import React from 'react'
 import { MotionDiv } from '@/utils/MotionDiv'
 import VerticalList from '@/components/home/VerticalList'
 import ContinueWatching from '@/components/home/ContinueWatching'
 import RecentEpisodes from '@/components/home/RecentEpisodes'
 import { getAuthSession } from './api/auth/[...nextauth]/route'
 import { redis } from '@/lib/rediscache'
-import RandomTextComponent from '@/components/RandomTextComponent'
-import MangaFeature from '@/components/home/MangaFeature'  // Import the MangaFeature component
+import RandomTextComponent from '@/components/RandomTextComponent';
+// import { getWatchHistory } from '@/lib/EpHistoryfunctions'
 
 async function getHomePage() {
   try {
     let cachedData;
     if (redis) {
-      cachedData = await redis.get('homepage');
+      cachedData = await redis.get(`homepage`);
       if (cachedData) {
         const parsedData = JSON.parse(cachedData);
         if (Object.keys(parsedData).length === 0) { // Check if data is an empty object
-          await redis.del('homepage');
+          await redis.del(`homepage`);
           cachedData = null;
         }
       }
     }
     if (cachedData) {
-      const { herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata } = JSON.parse(cachedData);
-      return { herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata };
+      const { herodata, populardata, top100data, seasonaldata } = JSON.parse(cachedData);
+      return { herodata, populardata, top100data, seasonaldata };
     } else {
-      const [herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata] = await Promise.all([
+      const [herodata, populardata, top100data, seasonaldata] = await Promise.all([
         TrendingAnilist(),
         PopularAnilist(),
         Top100Anilist(),
-        SeasonalAnilist(),
-        CategoriesAnilist(),
-        HeroAnilist()
+        SeasonalAnilist()
       ]);
       const cacheTime = 60 * 60 * 2;
       if (redis) {
-        await redis.set('homepage', JSON.stringify({ herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata }), 'EX', cacheTime);
+        await redis.set(`homepage`, JSON.stringify({ herodata, populardata, top100data, seasonaldata }), "EX", cacheTime);
       }
-      return { herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata };
+      return { herodata, populardata, top100data, seasonaldata };
     }
   } catch (error) {
     console.error("Error fetching homepage from anilist: ", error);
@@ -50,57 +48,40 @@ async function getHomePage() {
   }
 }
 
-function Home() {
-  const [session, setSession] = useState(null);
-  const [data, setData] = useState({
-    herodata: [],
-    populardata: [],
-    top100data: [],
-    seasonaldata: [],
-    categoriesdata: [],
-    herosectiondata: []
-  });
-
-  useEffect(() => {
-    async function fetchData() {
-      const sessionData = await getAuthSession();
-      setSession(sessionData);
-
-      const { herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata } = await getHomePage();
-      setData({ herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata });
-    }
-    fetchData();
-  }, []);
-
-  const { herodata, populardata, top100data, seasonaldata, categoriesdata, herosectiondata } = data;
+async function Home() {
+  const session = await getAuthSession();
+  const { herodata = [], populardata = [], top100data = [], seasonaldata = [] } = await getHomePage();
+  // const history = await getWatchHistory();
+  // console.log(history)
 
   return (
     <div>
       <Navbarcomponent home={true} />
-      <Herosection data={herosectiondata} />
+      <Herosection data={herodata} />
       <div className='sm:max-w-[97%] md:max-w-[95%] lg:max-w-[90%] xl:max-w-[85%] mx-auto flex flex-col md:gap-11 sm:gap-7 gap-5 mt-8'>
-        <div>
+        <div
+        >
           <ContinueWatching session={session} />
           <RandomTextComponent />
         </div>
-        <div>
+        <div
+        >
           <RecentEpisodes cardid="Recent Episodes" />
         </div>
-        <div>
+        <div
+        >
           <Animecard data={herodata} cardid="Trending Now" />
         </div>
-        <div>
+        <div
+        >
           <Animecard data={populardata} cardid="All Time Popular" />
         </div>
-        <div>
-          <MangaFeature /> {/* Add the MangaFeature component */}
-        </div>
-        <div>
-          <Animecard data={categoriesdata} cardid="Categories" />
-        </div>
-        <div className='lg:flex lg:flex-row justify-between lg:gap-20'>
-          <VerticalList data={top100data} mobiledata={seasonaldata} id="Top 100 Anime" />
-          <VerticalList data={seasonaldata} id="Seasonal Anime" />
+        <div
+        >
+          <div className='lg:flex lg:flex-row justify-between lg:gap-20'>
+            <VerticalList data={top100data} mobiledata={seasonaldata} id="Top 100 Anime" />
+            <VerticalList data={seasonaldata} id="Seasonal Anime" />
+          </div>
         </div>
       </div>
     </div>
