3967d9a874e57f242c2c0f7dcf27885382a8e1f7
Robert Jarrett <howtotech77@gmail.com>
2024-12-24 09:15:14 -0500

    Update DetailsContainer.js

--- src/app/anime/info/[...infoid]/DetailsContainer.js
+++ src/app/anime/info/[...infoid]/DetailsContainer.js
@@ -1,37 +1,42 @@
 "use client"
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Episodesection from '@/components/Episodesection'
 import AnimeDetailsTop from '@/components/details/AnimeDetailsTop'
+import MangaDetailsTop from '@/components/details/MangaDetailsTop'
 import AnimeDetailsBottom from '@/components/details/AnimeDetailsBottom'
 import Animecards from '@/components/CardComponent/Animecards'
 import { getUserLists } from '@/lib/AnilistUser';
 
-function DetailsContainer({data, id, session}) {
-    const [list,setList] = useState(null);
-    const [url, setUrl] = useState(null);
+function DetailsContainer({ data, id, session }) {
+  const [list, setList] = useState(null);
+  const [url, setUrl] = useState(null);
 
-    useEffect(() => {
-        const fetchlist = async()=>{
-            const data =await getUserLists(session?.user?.token, id);
-            setList(data);
-        }
-        fetchlist();
-    }, []);
+  useEffect(() => {
+    const fetchlist = async () => {
+      const data = await getUserLists(session?.user?.token, id);
+      setList(data);
+    }
+    fetchlist();
+  }, []);
 
-    const progress = list!==null ? list?.status==='COMPLETED' ? 0 : list?.progress : 0
+  const progress = list !== null ? list?.status === 'COMPLETED' ? 0 : list?.progress : 0;
 
   return (
     <>
       <div className='h-[500px] '>
-        <AnimeDetailsTop data={data} list={list} session={session} setList={setList} url={url}/>
+        {data?.type === 'MANGA' ? (
+          <MangaDetailsTop data={data} list={list} session={session} setList={setList} url={url} />
+        ) : (
+          <AnimeDetailsTop data={data} list={list} session={session} setList={setList} url={url} />
+        )}
       </div>
       <AnimeDetailsBottom data={data} />
-      <Episodesection data={data} id={id} setUrl={setUrl} progress={progress}/>
+      <Episodesection data={data} id={id} setUrl={setUrl} progress={progress} />
       {data?.recommendations?.nodes?.length > 0 && (
         <div className="recommendationglobal">
           <Animecards data={data.recommendations.nodes} cardid={"Recommendations"} />
         </div>
-      )} 
+      )}
     </>
   )
 }
