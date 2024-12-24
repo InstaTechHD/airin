0c75c72a99a1f9f083d0b490565ac1e6442d7bc6
Robert Jarrett <howtotech77@gmail.com>
2024-12-24 08:33:25 -0500

    Update DetailsContainer.js

--- src/app/anime/info/[...infoid]/DetailsContainer.js
+++ src/app/anime/info/[...infoid]/DetailsContainer.js
@@ -1,4 +1,4 @@
-"use client"
+"use client
 import React, { useState, useEffect } from 'react'
 import Episodesection from '@/components/Episodesection'
 import AnimeDetailsTop from '@/components/details/AnimeDetailsTop'
