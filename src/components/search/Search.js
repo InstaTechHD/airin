1d5f2937914b0e187aa62987d14335046d097763
Robert Jarrett <howtotech77@gmail.com>
2024-12-25 15:17:28 -0500

    Update Search.js

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -18,12 +18,13 @@ function Search() {
     const [loading, setLoading] = useState(false);
     const debouncedSearch = UseDebounce(query, 500);
     const [nextPage, setNextPage] = useState(false);
+    const [searchType, setSearchType] = useState("anime"); // State for toggling between anime and manga
 
     let focusInput = useRef(null);
 
     async function searchdata() {
         setLoading(true);
-        const res = await AdvancedSearch(debouncedSearch);
+        const res = await AdvancedSearch(debouncedSearch, searchType); // Pass searchType to the search function
         setData(res?.media)
         setNextPage(res?.pageInfo?.hasNextPage);
         console.log(res);
@@ -34,7 +35,7 @@ function Search() {
         if (debouncedSearch) {
             searchdata();
         }
-    }, [debouncedSearch]);
+    }, [debouncedSearch, searchType]); // Include searchType in the dependency array
 
     function closeModal() {
         useSearchbar.setState({ Isopen: false });
@@ -88,18 +89,31 @@ function Search() {
                                             <span>+</span>
                                             <div className="bg-[#1a1a1f] text-white text-xs font-bold px-2 py-1 rounded-md">S</div>
                                         </div>
-                                        <div className="mx-1 bg-[#1a1a1f] text-xs font-bold px-2 py-1 rounded-lg flex items-center justify-center">Anime</div>
+                                        <div className="mx-1 bg-[#1a1a1f] text-xs font-bold px-2 py-1 rounded-lg flex items-center justify-center">
+                                            {searchType === "anime" ? "Anime" : "Manga"}
+                                        </div>
+                                    </div>
+                                    <div className="flex justify-between py-2">
+                                        {/* Dropdown to toggle between anime and manga search */}
+                                        <select
+                                            value={searchType}
+                                            onChange={(e) => setSearchType(e.target.value)}
+                                            className="text-white bg-[#1a1a1f] rounded-md px-2 py-1"
+                                        >
+                                            <option value="anime">Anime</option>
+                                            <option value="manga">Manga</option>
+                                        </select>
                                     </div>
                                     <div className="flex items-center text-base font-medium rounded-lg bg-[#1a1a1f]">
                                         <Combobox.Input
                                             ref={focusInput}
                                             className="p-4 text-white w-full bg-transparent border-0 outline-none"
-                                            placeholder="Search Anime..."
+                                            placeholder={`Search ${searchType.charAt(0).toUpperCase() + searchType.slice(1)}...`}
                                             onChange={(event) => setQuery(event.target.value)}
                                             onKeyDown={(event) => {
                                                 if (event.key === "Enter") {
                                                     useSearchbar.setState({ Isopen: false });
-                                                    router.push(`/anime/catalog?search=${encodeURIComponent(event.target.value)}`);
+                                                    router.push(`/${searchType}/catalog?search=${encodeURIComponent(event.target.value)}`);
                                                     setData(null);
                                                     setQuery("");
                                                 }
@@ -122,7 +136,7 @@ function Search() {
                                                                 `flex items-center gap-3 py-[8px] px-5 border-b border-solid border-gray-800  ${active ? "bg-black/20 cursor-pointer" : ""
                                                                 }`
                                                             }>
-                                                            <Link href={`/anime/info/${item.id}`} onClick={() => { useSearchbar.setState({ Isopen: false }) }}>
+                                                            <Link href={`/${searchType}/info/${item.id}`} onClick={() => { useSearchbar.setState({ Isopen: false }) }}>
                                                                 <div className="shrink-0">
                                                                     <img
                                                                         src={item.image || item.coverImage.large}
@@ -133,7 +147,7 @@ function Search() {
                                                                     />
                                                                 </div>
                                                             </Link>
-                                                            <Link href={`/anime/info/${item.id}`} onClick={() => { useSearchbar.setState({ Isopen: false }) }}>
+                                                            <Link href={`/${searchType}/info/${item.id}`} onClick={() => { useSearchbar.setState({ Isopen: false }) }}>
                                                                 <div className="flex flex-col overflow-hidden">
                                                                     <p className="line-clamp-2 text-base">
                                                                         {item.title[animetitle] || item.title.romaji}
@@ -141,7 +155,7 @@ function Search() {
                                                                     <span className="my-1 text-xs text-gray-400">Episodes - {item?.episodes || item?.nextAiringEpisode?.episode - 1 || "?"}</span>
                                                                     <div className="flex items-center text-gray-400 text-xs">
                                                                         <span className="flex gap-1">
-                                                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 mt-[1pxd]" viewBox="0 0 1664 1600"><path fill="currentColor" d="M1664 615q0 22-26 48l-363 354l86 500q1 7 1 20q0 21-10.5 35.5T1321 1587q-19 0-40-12l-449-236l-449 236q-22 12-40 12q-21 0-31.5-14.5T301 1537q0-6 2-20l86-500L25 663Q0 636 0 615q0-37 56-46l502-73L783 41q19-41 49-41t49 41l225 455l502 73q56 9 56 46" /></svg>
+                                                                            <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 mt-[1px]" viewBox="0 0 1664 1600"><path fill="currentColor" d="M1664 615q0 22-26 48l-363 354l86 500q1 7 1 20q0 21-10.5 35.5T1321 1587q-19 0-40-12l-449-236l-449 236q-22 12-40 12q-21 0-31.5-14.5T301 1537q0-6 2-20l86-500L25 663Q0 636 0 615q0-37 56-46l502-73L783 41q19-41 49-41t49 41l225 455l502 73q56 9 56 46" /></svg>
                                                                             {item.averageScore / 10 || "0"}
                                                                         </span>
                                                                         <span className='mx-1 mb-[5px]'>.</span>
@@ -161,27 +175,23 @@ function Search() {
                                                             No results found.
                                                         </p>
                                                     )}
-                                                {data && nextPage && (
-                                                    <Link href={`/anime/catalog?search=${encodeURIComponent(query)}`}>
-                                                        <button
-                                                            type="button"
-                                                            onClick={() => {
-                                                                useSearchbar.setState({ Isopen: false });
-                                                                setQuery("");
-                                                            }}
-                                                            className="flex w-full items-center justify-center gap-2 py-4 transition duration-300 ease-in-out cursor-pointer border-none bg-[#4d148c] text-white text-base z-[5]">
-                                                            View Results
-                                                        </button>
-                                                    </Link>
-                                                )}
                                             </Fragment>
                                         ) : (
-                                            query !== "" &&
                                             <div className="flex items-center justify-center py-4">
-                                                Loading...
+                                                <span className="text-white">Loading...</span>
                                             </div>
                                         )}
                                     </Combobox.Options>
+                                    {nextPage && !loading && (
+                                        <div className="flex justify-center items-center py-4">
+                                            <button
+                                                onClick={() => searchdata()}
+                                                className="px-4 py-2 bg-primary text-white rounded-md"
+                                            >
+                                                Load more
+                                            </button>
+                                        </div>
+                                    )}
                                 </Combobox>
                             </Dialog.Panel>
                         </Transition.Child>
