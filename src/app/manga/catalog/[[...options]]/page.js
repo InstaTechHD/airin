b43012a2d3f65c748b16f9b86f92c4895d8c9437
Robert Jarrett <howtotech77@gmail.com>
2024-12-25 16:45:50 -0500

    Create page.js

---
+++ src/app/manga/catalog/[[...options]]/page.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Catalog from '@/components/catalogcomponent/Catalog'
+import Navbarcomponent from '@/components/navbar/Navbar'
+import Search from '@/components/search/Search'
+
+export async function generateMetadata({ params }) {
+  return {
+    title: "Makima - Manga Catalog",
+    openGraph: {
+      title: "Makima - Manga Catalog",
+    },
+    twitter: {
+      card: "summary",
+      title: "Makima - Manga Catalog",
+    },
+  }
+}
+
+function page({ searchParams }) {
+  return (
+    <div>
+      <Navbarcomponent />
+      <div className='max-w-[94%] xl:max-w-[88%] mx-auto mt-[70px]'>
+        <Catalog searchParams={searchParams} />
+      </div>
+      <Search />
+    </div>
+  )
+}
+
+export default page
