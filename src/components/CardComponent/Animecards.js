b81b98c04cf7f73c30fe63bca458ed7fba35f308
Robert Jarrett <howtotech77@gmail.com>
2024-12-26 00:49:56 -0500

    Update Animecards.js

--- src/components/CardComponent/Animecards.js
+++ src/components/CardComponent/Animecards.js
@@ -1,96 +1,98 @@
 "use client"
-"use strict"; // Add strict mode
-
-import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
-import styles from '../../styles/PlayAnimeCard.module.css';
+import React, { useRef, useState } from 'react';
+import styles from '../../styles/Animecard.module.css';
+import { useDraggable } from 'react-use-draggable-scroll';
 import Link from 'next/link';
-import { useTitle } from '../../lib/store';
-import { useStore } from "zustand";
+import ItemContent from './ItemContent';
 
-function PlayerAnimeCard({ data, id }) {
-  const animetitle = useStore(useTitle, (state) => state.animetitle);
-  const [visibleItems, setVisibleItems] = useState(4);
-  const episodesIcon = <svg viewBox="0 0 32 32" className="w-[18px] h-[18px] mr-1" fill="none" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M16 0C7.164 0 0 7.164 0 16s7.164 16 16 16 16-7.164 16-16S24.836 0 16 0zm0 29.091c-7.216 0-13.091-5.875-13.091-13.091S8.784 2.909 16 2.909 29.091 8.784 29.091 16 23.216 29.091 16 29.091z" fill="currentColor"/><path d="M11.636 22.182l-1.818-1.818 7.091-7.091V5.818h2.545v8.727l-8.818 8.818z" fill="currentColor"/></svg>;
+function Animecards({ data, cardid, show=true, type="anime" }) {
+  const containerRef = useRef();
+  const { events } = useDraggable(containerRef);
+  const [isLeftArrowActive, setIsLeftArrowActive] = useState(false);
+  const [isRightArrowActive, setIsRightArrowActive] = useState(false);
 
-  useEffect(() => {
-    if (id === 'Recommendations') {
-      setVisibleItems(15);
+  function handleScroll() {
+    const container = containerRef.current;
+    const scrollPosition = container.scrollLeft;
+    const maxScroll = container.scrollWidth - container.clientWidth;
+
+    setIsLeftArrowActive(scrollPosition > 30);
+    setIsRightArrowActive(scrollPosition < maxScroll - 30);
+  }
+
+  const smoothScroll = (amount) => {
+    const container = containerRef.current;
+    const cont = document.getElementById(cardid);
+
+    if (cont && container) {
+      cont.classList.add('scroll-smooth');
+      container.scrollLeft += amount;
+
+      setTimeout(() => {
+        cont.classList.remove('scroll-smooth');
+      }, 300);
     }
-  }, [data]);
-
-  const handleShowMore = () => {
-    setVisibleItems(data.length);
   };
 
-  const handleShowLess = () => {
-    setVisibleItems(4);
-  };
+  function scrollLeft() {
+    smoothScroll(-500);
+  }
+
+  function scrollRight() {
+    smoothScroll(+500);
+  }
 
   return (
-    <>
-      <div className='px-[10px] mb-[8px] mx-0 mt-0 leading-tight lg:px-[2px] lg:mx-2 flex items-center gap-2'>
-        <span className="h-6 md:h-8 rounded-md w-[.35rem] md:w-[.3rem] bg-white "></span>
-        <h2 className=" lg:text-[22px] text-[21px]">{id}</h2>
-      </div>
-      <div className={styles.playanimecard}>
-        {data && data?.slice(0, visibleItems).map((item) => (
-          <div className={styles.playcarditem} key={item?.node?.id || item?.mediaRecommendation?.id}>
-            <div className={styles.playcardimgcon}>
-              <Link href={`/${item?.node?.format?.toLowerCase() === 'manga' || item?.node?.format?.toLowerCase() === 'novel' ? 'manga/read' : 'anime/info'}/${item?.node?.id || item?.mediaRecommendation?.id}`}>
-                <Image
-                  src={item?.node?.coverImage?.large || item?.mediaRecommendation?.coverImage?.extraLarge}
-                  width={70}
-                  height={90}
-                  alt='image'
-                  className={styles.playcardimg}
-                />              
+    <div className={styles.animecard}>
+      {show && (
+        <div className={styles.cardhead}>
+          <span className={styles.bar}></span>
+          <h1 className={styles.headtitle}>{cardid}</h1>
+        </div>
+      )}
+      <div className={styles.animeitems}>
+        <span className={`${styles.leftarrow} ${isLeftArrowActive ? styles.active : styles.notactive}`}>
+          <svg onClick={scrollLeft} xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
+            <polyline points="15 18 9 12 15 6"></polyline>
+          </svg>
+        </span>
+        <span className={`${styles.rightarrow} ${isRightArrowActive ? styles.active : styles.notactive}`}>
+          <svg onClick={scrollRight} xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round">
+            <polyline points="9 18 15 12 9 6"></polyline>
+          </svg>
+        </span>
+        <div className={styles.cardcontainer} id={cardid} {...events} ref={containerRef} onScroll={handleScroll}>
+          {data?.map((item) => {
+            const media = {
+              id: item.id || '',
+              coverImage: item?.coverImage?.extraLarge || item?.coverImage?.large || '',
+              title: item.title || '',
+              status: item.status || '',
+              format: item.format || '',
+              episodes: item?.episodes || '',
+              chapters: item?.chapters || '',
+              nextAiringEpisode: item?.nextAiringEpisode || '',
+            };
+            return (
+              <Link href={`/${media.format === 'MANGA' || media.format === 'NOVEL' ? 'manga/read' : 'anime/info'}/${media.id}`} key={media.id}>
+                <ItemContent anime={media} cardid={cardid} />
               </Link>
-            </div>
-            <div className={styles.playcardinfo}>
-              <p className={styles.playcardrelation}>{item?.relationType}</p>
-              <Link href={`/${item?.node?.format?.toLowerCase() === 'manga' || item?.node?.format?.toLowerCase() === 'novel' ? 'manga/read' : 'anime/info'}/${item?.node?.id || item?.mediaRecommendation?.id}`}>
-                <p className={styles.playcardtitle}>{item?.node?.title?.[animetitle] || item?.mediaRecommendation?.title?.[animetitle] || item?.node?.title?.romaji || item?.mediaRecommendation?.title?.romaji}</p>
-              </Link>
-              <p className={styles.playepnum}>
-                {item?.node?.format || item?.mediaRecommendation?.format} <span>.</span>
-                {item?.node ? (
-                  item?.node?.episodes && (
-                    <>
-                      {episodesIcon}
-                      {item?.node?.episodes}
-                    </>
-                  ) ||
-                  (item?.node?.chapters && `${item?.node?.chapters} CH`) ||
-                  '?'
-                ) : (
-                  item?.mediaRecommendation?.episodes !== undefined && (
-                    <>
-                      {episodesIcon}
-                      {item?.mediaRecommendation?.episodes}
-                    </>
-                  )
-                )}
-                <span>.</span> {item?.node?.status || item?.mediaRecommendation?.status || ''}
-              </p>
-            </div>
-          </div>
-        ))}
-
-        {id !== 'Recommendations' && data.length > visibleItems && (
-          <div className={styles.showButton} onClick={handleShowMore}>
-            Show More
-          </div>
-        )}
-
-        {id !== 'Recommendations' && visibleItems > 5 && (
-          <div className={styles.showButton} onClick={handleShowLess}>
-            Show Less
-          </div>
-        )}
+            );
+          })}
+          {!data?.length && (
+            Array.from({ length: 15 }, (_, index) => (
+              <div key={index} className={`${styles.carditem} ${styles.loading}`}>
+                <div
+                  className={`${styles.cardimgcontainer} ${styles.pulse}`}
+                  style={{ animationDelay: `${(index + 2) * 0.3}s` }}
+                ></div>
+              </div>
+            ))
+          )}
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
-export default PlayerAnimeCard;
+export default Animecards;
