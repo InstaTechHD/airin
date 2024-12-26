0586ad78ec033ccca4d284c5f9c778924eedfc3f
Robert Jarrett <howtotech77@gmail.com>
2024-12-25 23:49:06 -0500

    Update hianimeApi.js

--- src/lib/hianimeApi.js
+++ src/lib/hianimeApi.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
 
+export async function fetchHianimeData(query) {
+  try {
+    const response = await axios.get(`https://aniwatch-api-flax.vercel.app/search`, {
+      params: { q: query }
+    });
+    return response.data.results;
+  } catch (error) {
+    console.error('Error fetching data from Hianime API:', error);
+    return [];
+  }
+}
