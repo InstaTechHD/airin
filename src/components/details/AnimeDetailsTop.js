dbb27e3354ecf01ce5062246a723678899f988c0
Robert Jarrett <howtotech77@gmail.com>
2024-12-26 01:22:12 -0500

    Update AnimeDetailsTop.js

--- src/components/details/AnimeDetailsTop.js
+++ src/components/details/AnimeDetailsTop.js
@@ -21,7 +21,7 @@ function AnimeDetailsTop({ data, list, session, setList, url }) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [mangaData, setMangaData] = useState(null);
 
-  const isAnime = data?.type !== 'MANGA';
+  const isAnime = data?.type === 'ANIME' || true;
 
   useEffect(() => {
     if (!isAnime) {
@@ -37,7 +37,12 @@ function AnimeDetailsTop({ data, list, session, setList, url }) {
     <div className={styles.detailsbanner}>
       <div
         className={styles.detailsbgimage}
-        style={{ backgroundImage: `url(${data?.bannerImage || data?.coverImage.extraLarge || null})`, backgroundPosition: "center", backgroundSize: "cover", height: "100%" }}
+        style={{
+          backgroundImage: `url(${data?.bannerImage || data?.coverImage.extraLarge || null})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          height: "100%",
+        }}
       ></div>
       <div className={styles.gradientOverlay}></div>
       <Button className={styles.detailstrailer} onPress={onOpen}>Watch Trailer</Button>
@@ -70,14 +75,14 @@ function AnimeDetailsTop({ data, list, session, setList, url }) {
           </h4>
           <p className={styles.scores}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[17px] h-[17px] mr-[2px]">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-[...]
+              <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
             </svg>
             {data?.averageScore / 10} | <span className={`${data?.status === 'RELEASING' ? styles.activestatus : styles.notactive}`}> {data?.status}</span>
           </p>
           <div className='flex'>
             {isAnime ? (
               <Link className={`${styles.detailswatch} ${!url && 'opacity-50 bg-black pointer-events-none'} hover:opacity-80 transition-all`} href={url ?? ''}>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-5 h-5 mr-1"><path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.3[...]
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" className="w-5 h-5 mr-1"><path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd"></path></svg>
                 {list !== null && list?.status === 'COMPLETED' ? 'Rewatch' : list !== null && list?.progress > 0 ? `Watch Ep ${list?.progress+1}` : `Play Now`}
               </Link>
             ) : (
@@ -98,7 +103,12 @@ function AnimeDetailsTop({ data, list, session, setList, url }) {
                     <div className='relative'>
                       <div
                         className="w-full !h-40 brightness-50 rounded-t-md"
-                        style={{ backgroundImage: `url(${data?.bannerImage || data?.coverImage.extraLarge || null})`, backgroundPosition: "center", backgroundSize: "cover", height: "100%", }}
+                        style={{
+                          backgroundImage: `url(${data?.bannerImage || data?.coverImage.extraLarge || null})`,
+                          backgroundPosition: "center",
+                          backgroundSize: "cover",
+                          height: "100%",
+                        }}
                       ></div>
                       <div className='absolute z-10 bottom-1 sm:bottom-0 sm:top-[65%] left-0 sm:left-3 md:left-10 flex flex-row items-center'>
                         <Image src={data?.coverImage?.extraLarge} alt='Image' width={120} height={120} className="hidden sm:flex rounded-md" priority={true}/>
