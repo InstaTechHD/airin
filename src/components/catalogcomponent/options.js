e5de935b0402b41f71fc2fedaa1e056cdf6b7b6b
Robert Jarrett <howtotech77@gmail.com>
2024-12-27 12:08:15 -0500

    Update options.js

--- src/components/catalogcomponent/options.js
+++ src/components/catalogcomponent/options.js
@@ -153,6 +153,7 @@
     { name: "Special", value: "SPECIAL" },
     { name: "OVA", value: "OVA" },
     { name: "ONA", value: "ONA" },
+    { name: "Music", value: "Music" },
   ];
 
   export const sortbyOptions = [
@@ -192,6 +193,12 @@
     { name: "2022", value: "2022" },
     { name: "2023", value: "2023" },
     { name: "2024", value: "2024" },
+    { name: "2025", value: "2025" },
+    { name: "2026", value: "2026" },
+    { name: "2027", value: "2027" },
+    { name: "2028", value: "2028" },
+    { name: "2029", value: "2029" },
+    { name: "2030", value: "2030" },
   ];
   export const seasonOptions = [
     { name: "Winter", value: "WINTER" },
