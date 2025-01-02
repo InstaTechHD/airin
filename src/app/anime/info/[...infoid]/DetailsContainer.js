a248b826fa5e8788d166824467052153852332e1
Robert Jarrett <howtotech77@gmail.com>
2025-01-01 19:06:38 -0500

    Update DetailsContainer.js

--- src/app/anime/info/[...infoid]/DetailsContainer.js
+++ src/app/anime/info/[...infoid]/DetailsContainer.js
@@ -39,9 +39,9 @@ function DetailsContainer({ data, id, session, type }) {
     <>
       <div className="h-[500px] ">
         <AnimeDetailsTop data={data} list={list} session={session} setList={setList} url={url} />
-          <div className="watch2gether-button">
-        <button onClick={handleCreateRoom}>Watch Together</button>
       </div>
+    <div className="watch2gether-button">
+        <button onClick={handleCreateRoom}>Watch Together</button>
       </div>
       <AnimeDetailsBottom data={data} />
       <Episodesection data={data} id={id} setUrl={setUrl} progress={progress} />
