4fe8a184884f7001019500cf8210687043683c6c
Robert Jarrett <howtotech77@gmail.com>
2025-01-03 14:34:17 -0500

    Update RandomTextComponent.js

--- src/components/RandomTextComponent.js
+++ src/components/RandomTextComponent.js
@@ -6,21 +6,22 @@ const RandomTextComponent = () => {
   // Array of random texts with associated links
   const randomTexts = [
     {
-      text: "Enjoying Makima? Help us keep our site alive by donating or sharing the app!",
-      link: "https://ko-fi.com/makimadon",
+      text: "", // No text needed as we are displaying a banner
+      link: "https://ko-fi.com/usemakima",
+      image: "https://raw.githubusercontent.com/InstaTechHD/airin/refs/heads/master/public/kofi-support.png"
     },
-    {
-      text: "Have you tried customizing the app to your liking? Try it if you haven't!",
-      link: "/settings",
-    },
-    {
-      text: "Did you know? Makima secures your AniList account when you sync?",
-      link: "/user/profile",
-    },
-    {
-      text: "Join our Discord for some exciting events, giveaways and more!",
-      link: "https://discord.gg/zvD8xDhqzu",
-    },
+    // {
+    //   text: "Have you tried customizing the app to your liking? Try it if you haven't!",
+    //   link: "/settings",
+    // },
+    // {
+    //   text: "Did you know? Makima secures your AniList account when you sync?",
+    //   link: "/user/profile",
+    // },
+    // {
+    //   text: "Join our Discord for some exciting events, giveaways and more!",
+    //   link: "https://discord.gg/zvD8xDhqzu",
+    // },
   ];
 
   // Generate a random text
@@ -47,12 +48,16 @@ const RandomTextComponent = () => {
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+          d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09Z"
         />
       </svg>
       <p className="text-[11px] text-[#bfc6d0] lg:max-w-[65%] line-clamp-3">
         <a className="text-blue-500 hover:text-blue-600" href={randomText.link}>
-          {randomText.text}
+          {randomText.image ? (
+            <img src={randomText.image} alt="Support me on Ko-fi" className="kofi-banner" />
+          ) : (
+            randomText.text
+          )}
         </a>
       </p>
     </div>
