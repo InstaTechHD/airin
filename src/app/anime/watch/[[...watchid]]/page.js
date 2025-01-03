f5ff75b994977213477d097eca7d8531122203f2
Robert Jarrett <howtotech77@gmail.com>
2025-01-03 18:08:23 -0500

    Update page.js

--- src/app/anime/watch/[[...watchid]]/page.js
+++ src/app/anime/watch/[[...watchid]]/page.js
@@ -2,16 +2,11 @@
 
 import React from "react";
 import { AnimeInfoAnilist } from '@/lib/Anilistfunctions';
-import NextAiringDate from "@/components/videoplayer/NextAiringDate";
-import PlayerAnimeCard from "@/components/videoplayer/PlayerAnimeCard";
 import Navbarcomponent from "@/components/navbar/Navbar";
-import PlayerComponent from "@/components/videoplayer/PlayerComponent";
-import Animecards from "@/components/CardComponent/Animecards";
 import { createWatchEp, getEpisode } from "@/lib/EpHistoryfunctions";
-import { WatchPageInfo } from "@/lib/AnilistUser";
 import { getAuthSession } from "../../../api/auth/[...nextauth]/route";
 import { redis } from '@/lib/rediscache';
-import CommentSection from "@/components/CommentSection"; // Import the new component
+import AnimeWatchClient from "./AnimeWatchClient"; // Import the client component
 
 async function getInfo(id) {
   try {
@@ -93,30 +88,17 @@ async function AnimeWatch({ params, searchParams }) {
   return (
     <>
       <Navbarcomponent />
-      <div className="w-full flex flex-col lg:flex-row lg:max-w-[98%] mx-auto xl:max-w-[94%] lg:gap-[6px] mt-[70px]">
-        <div className="flex-grow w-full h-full">
-          <PlayerComponent id={id} epId={epId} provider={provider} epNum={epNum} data={data} subdub={subdub} session={session} savedep={savedep} />
-          {data?.status === 'RELEASING' &&
-            <NextAiringDate nextAiringEpisode={data?.nextAiringEpisode} />
-          }
-          {/* Comment section */}
-          <CommentSection comments={comments} />
-        </div>
-        <div className="h-full lg:flex lg:flex-col md:max-lg:w-full gap-10">
-          <div className="rounded-lg hidden lg:block lg:max-w-[280px] xl:max-w-[380px] w-[100%] xl:overflow-y-scroll xl:overflow-x-hidden overflow-hidden scrollbar-hide overflow-y-hidden">
-            <PlayerAnimeCard data={data?.relations?.edges} id="Related Anime" />
-          </div>
-          <div className="rounded-lg hidden lg:block lg:max-w-[280px] xl:max-w-[380px] w-[100%] xl:overflow-y-scroll xl:overflow-x-hidden overflow-hidden scrollbar-hide overflow-y-hidden">
-            <PlayerAnimeCard data={data?.recommendations?.nodes} id="Recommendations" />
-          </div>
-        </div>
-        <div className="lg:hidden">
-          <Animecards data={data?.relations?.edges} cardid="Related Anime" />
-        </div>
-        <div className="lg:hidden">
-          <Animecards data={data?.recommendations?.nodes} cardid={"Recommendations"} />
-        </div>
-      </div>
+      <AnimeWatchClient
+        id={id}
+        epId={epId}
+        provider={provider}
+        epNum={epNum}
+        data={data}
+        subdub={subdub}
+        session={session}
+        savedep={savedep}
+        comments={comments}
+      />
     </>
   );
 }
