021fcdb478d5ad3bd4970020e617a2c305de7b02
Robert Jarrett <howtotech77@gmail.com>
2025-01-03 18:41:29 -0500

    Update route.js

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,9 +1,23 @@
 import { getServerSession } from "next-auth";
-import {NextResponse} from 'next/server';
+import { NextResponse } from 'next/server';
 import { authOptions } from "./[...nextauth]/route";
 
-export async function GET(req){
+export async function GET(req) {
     const session = await getServerSession(authOptions);
-    console.log(session)
-    return NextResponse.json({id:1})
+
+    // Check if the user is authenticated
+    if (session) {
+        // User is authenticated
+        return NextResponse.json({
+            success: true,
+            message: "User is authenticated",
+            session: session,
+        });
+    } else {
+        // User is not authenticated
+        return NextResponse.json({
+            success: false,
+            message: "User is not authenticated",
+        });
+    }
 }
